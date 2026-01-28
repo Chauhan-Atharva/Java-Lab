009f3f086775039dd7a87a21c65003c4ef22b466
Chauhan-Atharva <atharva.st.05@gmail.com>
2026-01-28 11:03:38 +0000

    lab work 28 jan

--- one.java
+++ one.java
@@ -1,6 +1,6 @@
 import  java.util.*; //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class one {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
